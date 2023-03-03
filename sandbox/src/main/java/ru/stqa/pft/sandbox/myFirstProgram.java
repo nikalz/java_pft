2e8e60553415c2ba0872a058136e882c91074321
nikalz <126697079+nikalz@users.noreply.github.com>
2023-03-03 15:49:22 +0200

    rename

--- sandbox/src/main/java/ru/stqa/pft/sandbox/myFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/myFirstProgram.java
@@ -3,6 +3,8 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
     public static void main(String[] args) {
         System.out.println("Hi all");
+
+        System.out.println(2+2);
     }
 
 }
