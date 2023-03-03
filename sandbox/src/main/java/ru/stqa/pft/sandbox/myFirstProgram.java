42d96ddbc076aabb12a4fb7d8b0e20afae5f490d
nikalz <126697079+nikalz@users.noreply.github.com>
2023-03-03 17:17:52 +0200

    lesson 1.13

--- sandbox/src/main/java/ru/stqa/pft/sandbox/myFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/myFirstProgram.java
@@ -2,9 +2,25 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
     public static void main(String[] args) {
-        System.out.println("Hi all");
-
-        System.out.println(2+2);
+        hello("ymnik");
+//        double i = 5;
+//        double k = i *i;
+//        System.out.println(k + i);
+        double i = 5;
+        System.out.println("Ploshad kvadrata= " + area(i));
+        double b = 6;
+        System.out.println("Ploshad pryamoyg= " + area(i,b));
     }
 
+    public static void hello (String who) {
+
+        System.out.println("Hi " + who);
+
+    }
+    public static double area (double l){
+        return l * l;
+    }
+    public static double area (double l, double h){
+        return l * h;
+    }
 }
