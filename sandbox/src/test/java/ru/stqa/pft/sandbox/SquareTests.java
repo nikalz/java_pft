59cd2a89ee633a401598fe0dafd4980531cbdfcf
nikalz <126697079+nikalz@users.noreply.github.com>
2023-03-06 18:02:03 +0200

    try go

--- sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
@@ -1,12 +1,13 @@
 package ru.stqa.pft.sandbox;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SquareTests {
 
     @Test
-    public void testArea(){
-        Square  s = new Square(5);
-        assert s.area() == 25;
+    public void testArea () {
+        Square s = new Square(5);
+        Assert.assertEquals( s.area(), 24);
     }
 }
